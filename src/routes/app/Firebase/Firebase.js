2591431049e6216e6ad9e93a1ac72b27028f9126
Your-name <amine.addajou@gmail.com>
2022-06-25 11:28:02 +0200

    config to prod

--- src/routes/app/Firebase/Firebase.js
+++ src/routes/app/Firebase/Firebase.js
@@ -4,8 +4,8 @@ import { getAuth } from 'firebase/auth';
 import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDy-ahf11eteYRB0s11DkzTwcP2ftxM26E",
-    authDomain: "green-collecte.firebaseapp.com",
+    apiKey: "",
+    authDomain: "",
     projectId: process.env.REACT_APP_projectId,
     storageBucket: process.env.REACT_APP_storageBucket,
     messagingSenderId: process.env.REACT_APP_messagingSenderId,
